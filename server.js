e0a626083a05c311bf46093ad38eebf23ad2cb1d
Matti Aho <ookooko@gmail.com>
2018-08-30 19:57:35 +0300

    profileId, image

--- server.js
+++ server.js
@@ -29,6 +29,39 @@ app.get('/', (req, res) => {
   res.send(database.users);
 });
 
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.status(200).json(user);
+    }
+  });
+  
+  if (!found) {
+    res.status(400).json('not found');
+  }
+});
+
+app.put('/image/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  
+  if (!found) {
+    res.status(400).json('not found');
+  }
+});
+
 app.post('/signin', (req, res) => {
   if (req.body.email === database.users[0].email && 
       req.body.password === database.users[0].password) {
