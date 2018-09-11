f7ec14ba1b7d3c264315e3a702e7364f2908b8dd
Matti Aho <ookooko@gmail.com>
2018-09-11 18:52:58 +0300

    endpoits, controlles

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const knex = require('knex');
 const bcrypt = require('bcrypt-nodejs');
 const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,58 +22,10 @@ const db = knex({
   }
 });
 
-app.get('/', (req, res) => {
-  res.send(database.users);
-});
-
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-    
-  db.select('*')
-      .from('users')
-      .where({id})
-      .then(user => {
-        if (user.length) {
-            res.json(user[0]);
-        } else {
-            res.status(400).json('not found')
-        }
-      })
-      .catch(err => res.status(400).json('error getting user'))
-});
-
-app.put('/image/:id', (req, res) => {
-  const { id } = req.params;
-
-  db('users')
-    .where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => res.json(entries[0]))
-    .catch(err => res.status(400).json('unable to get entries'))
-});
-
-app.post('/signin', (req, res) => {
-    db.select('email', 'hash')
-    .from('login')
-    .where('email','=',req.body.email)
-    .then(data => {
-        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-        if (isValid) { 
-            return db.select('*')
-              .from('users')
-              .where('email','=',req.body.email)
-              .then(user => {
-                res.json(user[0]);
-            })
-            .catch(err => res.status(400).json('unable to get user'))
-        } else {
-            res.status(400).json('wrong credentials');
-        }
-    })
-    .catch(err => res.status(400).json('wrong credentials'))
-});
-
+app.get('/', (req, res) => res.send(database.users));
+app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
+app.put('/image/:id', (req, res) => image.handleImage(req, res, db));
+app.post('/signin', (req, res) => signin.handleSignin(req, res, db, bcrypt));
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
 
 app.listen(3000, () => {
